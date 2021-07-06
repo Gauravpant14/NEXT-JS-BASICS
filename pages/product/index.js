0b7c20ca57e18242745bc5abe6d3bb0bbcd36d9b
Gaurav <aadeezcity@gmail.com>
2021-07-06 11:15:12 +0530

    link component navigation

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import Link from 'next/link'
 const ProductList = () => {
     return (
         <>
-          <h2>Product 1</h2>  
-          <h2>Product 2</h2>  
-          <h2>Product 3</h2>  
+        <Link href="/">
+        <a>Home Page</a>
+        </Link>
+        <Link href="/product/1">
+        <a> <h2>Product 1</h2> </a>
+        </Link>
+        <Link href="/product/2">
+        <a> <h2>Product 2</h2> </a>
+        </Link>
+
+      {/* Replace Prop will replace */}
+        
+        <Link href="/product/3" replace>
+        <a> <h2>Product 3</h2> </a>
+        </Link>
+           
+          
         </>
     )
 }
